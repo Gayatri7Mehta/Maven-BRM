7e365586bd6b130ec97481dfa17cafa92e2f9a2e
Gayatri <gayatri29mehta@gmail.com>
2023-02-08 16:46:41 +0530

    test

--- src/main/java/pkg4/MyClass.java
+++ src/main/java/pkg4/MyClass.java
@@ -4,11 +4,14 @@ public class MyClass {
 	public void show() {
 		System.out.println("Welcome to Jenkins");
 	}
+	public void display() {
+		System.out.println("Hello");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyClass obj= new MyClass();
-		obj.show();
+		obj.show() ;
 
 	}
 
