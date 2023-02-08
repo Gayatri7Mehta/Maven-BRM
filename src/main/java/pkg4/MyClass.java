ca65915cad9e81753c16e7efecb574f1b31c659a
Gayatri <gayatri29mehta@gmail.com>
2023-02-08 15:30:33 +0530

    first commit

---
+++ src/main/java/pkg4/MyClass.java
@@ -0,0 +1,15 @@
+package pkg4;
+
+public class MyClass {
+	public void show() {
+		System.out.println("Welcome to Jenkins");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass obj= new MyClass();
+		obj.show();
+
+	}
+
+}
